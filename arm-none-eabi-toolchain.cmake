95d207060cfd524cb267ce9c0e6ef4279b5121c1
shovdmi <shovdmi@yandex.ru>
2026-01-01 16:22:03 +0300

    Separated arm-none-eabi-toolchain.cmake

--- arm-none-eabi-toolchain.cmake
+++ arm-none-eabi-toolchain.cmake
@@ -1,8 +1,20 @@
 #-- System ---------------------------------------------------------------------
 SET(CMAKE_SYSTEM_NAME Generic)
 SET(CMAKE_SYSTEM_PROCESSOR arm)
+set(CMAKE_CROSSCOMPILING 1)
+set(CMAKE_C_STANDARD 11)
+set(CMAKE_CXX_STANDARD 11)
+
+#  Without this flag, CMake is unable to pass the test compilation check
+set(CMAKE_TRY_COMPILE_TARGET_TYPE "STATIC_LIBRARY")
+#set(CMAKE_C_COMPILER_WORKS TRUE)
+#set(CMAKE_CXX_COMPILER_WORKS TRUE)
+# To fix compilation check: enable either these or CMAKE_TRY_COMPILE_TARGET_TYPE flag
+#set(CMAKE_C_COMPILER_FORCED TRUE)
+#set(CMAKE_CXX_COMPILER_FORCED TRUE)
 
 #-- Toolchain ------------------------------------------------------------------
+# see https://stackoverflow.com/a/17275650 about `CACHE PATH "" FORCE `
 set(TOOLCHAIN_PATH  "")
 SET(CMAKE_C_COMPILER   ${TOOLCHAIN_PATH}arm-none-eabi-gcc)
 SET(CMAKE_CXX_COMPILER ${TOOLCHAIN_PATH}arm-none-eabi-g++)
@@ -12,22 +24,6 @@ SET(CMAKE_OBJCOPY      ${TOOLCHAIN_PATH}arm-none-eabi-objcopy)
 SET(CMAKE_OBJDUMP      ${TOOLCHAIN_PATH}arm-none-eabi-objdump)
 SET(CMAKE_SIZE         ${TOOLCHAIN_PATH}arm-none-eabi-size   )
 
-
-#  Without this flag, CMake is unable to pass the test compilation check
-set(CMAKE_TRY_COMPILE_TARGET_TYPE "STATIC_LIBRARY")
-# To fix compilation check: enable either these or CMAKE_TRY_COMPILE_TARGET_TYPE flag
-#set(CMAKE_C_COMPILER_FORCED TRUE)
-#set(CMAKE_CXX_COMPILER_FORCED TRUE)
-
 ENABLE_LANGUAGE(C)
 ENABLE_LANGUAGE(CXX)
 ENABLE_LANGUAGE(ASM)
-
-
-#-- Common flags ---------------------------------------------------------------
-SET(COMMON_FLAGS "-mthumb -mcpu=${MCU_CORE} -nostdlib --specs=nosys.specs --specs=nano.specs -g3 -ggdb")
-SET(COMPILER_COMMON_FLAGS "-Wall -Wextra -ffunction-sections -fdata-sections")
-SET(CMAKE_C_FLAGS   "${COMMON_FLAGS} ${COMPILER_COMMON_FLAGS} -std=c11"  )
-SET(CMAKE_CXX_FLAGS "${COMMON_FLAGS} ${COMPILER_COMMON_FLAGS} -std=c++11")
-SET(CMAKE_ASM_FLAGS "-x assembler-with-cpp -O0 -g3 -ggdb -mthumb -mcpu=${MCU_CORE}")
-SET(CMAKE_LINKER_FLAGS "${COMMON_FLAGS} -Wl,--gc-sections -g")
